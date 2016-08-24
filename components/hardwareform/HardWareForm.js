ca6cfbc7a9c90c6e19ca267609e575ffd7bc6db7
zhukovka <helen.zhukova@gmail.com>
2016-08-25 00:21:41 +0300

    hardware form

---
+++ components/hardwareform/HardWareForm.js
@@ -0,0 +1,39 @@
+import React, {PropTypes} from 'react';
+import VGComponentForm from '../vgcomponentform';
+import {CardActionsRight} from 'react-toolbox-addons/lib/cardactionsright';
+import {Button} from 'react-toolbox/lib/button';
+import {Row} from 'react-toolbox-addons/lib/grid';
+import HardWare from '../../classes/HardWare.ts';
+import FormActions from '../../classes/FormActions.ts';
+
+class HardWareForm extends VGComponentForm {
+    static propTypes = {
+        info: PropTypes.instanceOf(HardWare),
+        hardwareActions: PropTypes.instanceOf(FormActions)
+    };
+
+    constructor (props) {
+        super(props);
+        const {type, manufacturer, model, uid} = this.props.info;
+        this.state = this.setInitialState([type, manufacturer, model, uid]);
+    }
+
+    renderFormContent () {
+        const {type, manufacturer, model, uid} = this.props.info;
+        const {cancel, finish} = this.props.hardwareActions;
+        return (
+            <fieldset>
+                <Row expanded>
+                    {this.renderDropDowns([type, manufacturer, model])}
+                    {this.renderInput(uid)}
+                </Row>
+                <CardActionsRight>
+                        <Button {...cancel}/>
+                        <Button {...finish}/>
+                </CardActionsRight>
+            </fieldset>
+        );
+    }
+}
+
+export default HardWareForm;
