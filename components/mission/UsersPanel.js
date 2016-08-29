aef88c7fa7a8d8ab472fed34d8d5652e7af9192c
zhukovka <helen.zhukova@gmail.com>
2016-08-29 20:31:29 +0300

    UserPanel to UsersPanel

--- components/mission/UsersPanel.js
+++ components/mission/UsersPanel.js
@@ -1,15 +1,15 @@
 import React, {PropTypes} from 'react';
 import NavDrawerPlus from 'react-toolbox-addons/lib/nawdrawerplus';
 
-const UserPanel = ({children, open, onClick})=> {
+const UsersPanel = ({children, open, onClick})=> {
     return (
         <NavDrawerPlus title="Users" active={open} pinned={open} onClick={onClick}>
             {children}
         </NavDrawerPlus>);
 };
-UserPanel.propTypes = {
+UsersPanel.propTypes = {
     children: PropTypes.any,
     onClick: PropTypes.func,
     open: PropTypes.bool
 };
-export default UserPanel;
+export default UsersPanel;
