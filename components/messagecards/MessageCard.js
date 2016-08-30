1051619e6b7018677e5cf9781d039b3132db87d3
zhukovka <helen.zhukova@gmail.com>
2016-08-30 17:41:25 +0300

    Messagecard

--- components/messagecards/MessageCard.js
+++ components/messagecards/MessageCard.js
@@ -1,24 +1,43 @@
 import React, {PropTypes} from 'react';
-import {Card, CardMedia} from 'react-toolbox/lib/card';
-import {Row, Col} from 'react-toolbox-addons/lib/grid';
+import {Card, CardText} from 'react-toolbox/lib/card';
 import Logo from '../logo/Logo';
 import {CardMediaPrimary} from 'react-toolbox-addons/lib/card-addons';
 
 /**
  *  MessageCard
+ *  example in example.md (https://github.com/zhukovka/vg-react-toolbox/tree/master/components/messagecard)
  */
-const MessageCard = ({image})=> {
+const MessageCard = ({image, message, title, children})=> {
     return (
         <Card>
             <CardMediaPrimary>
-                <Logo image={image}/>
+                <div style={{margin: '5rem 0'}}>
+                    <Logo image={image} />
+                </div>
             </CardMediaPrimary>
+            <CardText style={{textAlign: 'center'}}>
+                <h3>{title}</h3>
+                <h5>{message}</h5>
+            </CardText>
+            {children}
         </Card>
     );
 };
 
 MessageCard.propTypes = {
-    image: PropTypes.string
+    children: PropTypes.any,
+    /**
+     * <String> image - url to use as the logo
+     */
+    image: PropTypes.string,
+    /**
+     * <String> message - message to show as a card text
+     */
+    message: PropTypes.string,
+    /**
+     * <String> title - card title
+     */
+    title: PropTypes.string
 };
 
 export default MessageCard;
---
+++ components/messagecards/example.md
@@ -0,0 +1,25 @@
+# MessageCard
+
+### Example
+
+```
+import React from 'react';
+import MessageCard from 'vg-react-toolbox/lib/messagecards/MessageCard';
+import {CardActionsSpaced} from 'react-toolbox-addons/lib/cardactionsspaced';
+import {Button} from 'react-toolbox/lib/button';
+const MessageCardTest = ()=>{
+  return (
+      <section>
+          <h5>MessageCard</h5>
+          <MessageCard image="https://placeimg.com/120/120/animals" message="Organization profile has been created successfully" title="Congratulations!">
+              <CardActionsSpaced>
+                  <Button label='BACK TO ORANIZATION LIST' primary />
+                  <Button label='VIEW ORANIZATION PAGE' primary />
+              </CardActionsSpaced>
+          </MessageCard>
+      </section>
+  );
+};
+
+export default MessageCardTest;
+```
