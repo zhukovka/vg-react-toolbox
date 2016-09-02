2667f0e822717d9700383f7e1a62acecc4b97ed4
zhukovka <helen.zhukova@gmail.com>
2016-09-02 18:16:20 +0300

    mission list

--- spec/mission/root.js
+++ spec/mission/root.js
@@ -2,6 +2,8 @@ import 'react-toolbox/lib/commons.scss';
 import React from 'react';
 import MissionPage from '../MissionPage/index';
 
-const Root = () => (<MissionPage/>);
+const Root = () => (<div>
+    <MissionPage/>
+</div>);
 
 export default Root;
